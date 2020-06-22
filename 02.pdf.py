946f313dd78c3f878ccc7698478411be9d4733b2
izonejangwonyoung <2000shim@naver.com>
2020-06-22 20:50:22 +0900

    오리지널

---
+++ 02.pdf.py
@@ -0,0 +1,12 @@
+# x = 100
+# y = 20
+# print(“x+y=”, x+y)
+
+s = x + y
+print(“(2) sum = ”, s)
+# x = 10
+# print (“(3) x + y = ”,x+y)
+# print (“(4) sum =”,s)
+# x=“100”
+# y=“20”
+# print(x+y)
---
+++ turtle.py
@@ -0,0 +1,13 @@
+
+import turtle
+t = turtle.Turtle()
+t.shape(“turtle")
+ra = 200
+t.forward(ra)
+t.left(90)
+t.forward(ra)
+t.left(90)
+t.forward(ra)
+t.left(90)
+t.forward(ra)
+t.left(90)
