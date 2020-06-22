2d906eaa2194c6f4fec56d8b97d477ece4571337
izonejangwonyoung <2000shim@naver.com>
2020-06-22 20:21:00 +0900

    edit version

--- age.py
+++ age.py
@@ -1,3 +1,3 @@
-age=int(input("HOW OLD ARE YOU(in korean age)?"))
-print("you stay this planet since",age-1,ago...)
+age=int(input("HOW OLD ARE YOU(in korean age)?::"))
+print("you stay this planet since",age-1,"years ago...")
 
