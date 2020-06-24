eb3cc2bb54c289ce89144e3705fa0a5a8ba59bf0
izonejangwonyoung <2000shim@naver.com>
2020-06-24 18:17:40 +0900

    재밌게 만듦

--- 승부차기.py
+++ 승부차기.py
@@ -11,8 +11,8 @@ user_choice=input("어디를 수비하시겠어요?::")
 while True:
     if computer_choice==user_choice:
         print("수비에 성공하셨습니다. 골키퍼는",user_choice,"로 힘차게 다이빙했고, 성공적으로 방어했습니다.")
-
+        break
 
     if computer_choice!= user_choice:
         print("페널티 킥이 성공하였습니다.",computer_choice,"로 정확하게 꽂아넣습니다!")
-
+        break
